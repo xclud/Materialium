4816ff9e52fab52ae37232dff0de086663b16746
Mahdi <mahdi3466@yahoo.com>
2019-03-17 15:56:03 +0330

    Ports FormField and TopAppBarTitle components to C#.

---
+++ src/Materialium/FormField.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class FormField : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            int n = 0;
+
+            builder.OpenElement(n++, "div");
+            AddCommonAttributes(builder, ref n);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        bool AlignEnd { get; set; }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-form-field";
+
+            if (AlignEnd)
+            {
+                yield return "mdc-form-field--align-end";
+            }
+        }
+    }
+}
---
+++ src/Materialium/TopAppBarTitle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class TopAppBarTitle : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            int n = 0;
+            builder.OpenElement(n++, "span");
+            AddCommonAttributes(builder, ref n);
+            builder.CloseElement();
+        }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-top-app-bar__title";
+        }
+    }
+}
