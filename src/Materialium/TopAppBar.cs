3e20f8017e5cdbaa0ceefd7511b07515f31605b1
Mahdi <mahdi3466@yahoo.com>
2019-03-22 14:40:10 +0430

    TopAppBar component ported to C#.

---
+++ src/Materialium/TopAppBar.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Materialium
+{
+    public class TopAppBar : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "header");
+            CaptureElementReference(builder, ref n);
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        bool Fixed { get; set; }
+
+        [Parameter]
+        bool Short { get; set; }
+
+        [Parameter]
+        bool Collapsed { get; set; }
+
+        [Parameter]
+        bool Prominent { get; set; }
+
+        [Parameter]
+        bool Dense { get; set; }
+
+        [Parameter]
+        string ScrollTarget { get; set; }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-top-app-bar";
+
+            if (Fixed)
+            {
+                yield return "mdc-top-app-bar--fixed";
+            }
+
+            if (Short)
+            {
+                yield return "mdc-top-app-bar--short";
+            }
+
+            if (Collapsed)
+            {
+                yield return "mdc-top-app-bar--short-collapsed";
+            }
+
+            if (Prominent)
+            {
+                yield return "mdc-top-app-bar--prominent";
+            }
+
+            if (Dense)
+            {
+                yield return "mdc-top-app-bar--dense";
+            }
+        }
+
+        bool isFirstRender = true;
+
+        protected override async Task OnAfterRenderAsync()
+        {
+            if (isFirstRender)
+            {
+                try
+                {
+                    await jsRuntime.InvokeAsync<object>("Materialium.topAppBar.init", element, ScrollTarget);
+                }
+                catch { }
+
+                isFirstRender = false;
+            }
+        }
+
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IJSRuntime jsRuntime { get; set; }
+    }
+}
