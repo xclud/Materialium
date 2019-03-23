fff1ecd4115d3763fbd62f3450e075aa89340872
Mahdi <mahdi3466@yahoo.com>
2019-03-23 19:28:08 +0430

    TopAppBarNavigationIcon ported to C#.

---
+++ src/Materialium/TopAppBarNavigationIcon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class TopAppBarNavigationIcon : MaterialComponentBase
+    {
+        public TopAppBarNavigationIcon()
+        {
+            Class = "material-icons";
+        }
+
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "button");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-top-app-bar__navigation-icon";
+        }
+    }
+}
