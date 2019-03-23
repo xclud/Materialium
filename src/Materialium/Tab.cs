17ccd0647deaff35ee3928e361e60ea88edd8a51
Mahdi <mahdi3466@yahoo.com>
2019-03-23 20:30:23 +0430

    Tab ported to C#.

---
+++ src/Materialium/Tab.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class Tab : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "button");
+
+            builder.AddAttribute(n++, "role", "tab");
+
+            if (Active)
+            {
+                builder.AddAttribute(n++, "aria-selected", Active);
+            }
+
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        bool Active { get; set; }
+
+        [Parameter]
+        bool Stacked { get; set; }
+
+        [Parameter]
+        bool MinWidth { get; set; }
+
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-tab";
+
+            if (Active)
+            {
+                yield return "mdc-tab--active";
+            }
+            if (Stacked)
+            {
+                yield return "mdc-tab--stacked";
+            }
+
+            if (MinWidth)
+            {
+                yield return "mdc-tab--min-width";
+            }
+        }
+    }
+}
