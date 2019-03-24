48a9c335d112415c2c8f4491dcf2b69a3a9ec077
Mahdi <mahdi3466@yahoo.com>
2019-03-25 01:05:16 +0430

    Card and friends ported to C#.

---
+++ src/Materialium/CardMedia.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class CardMedia : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "div");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        CardMediaRatio Ratio { get; set; }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-card__media";
+
+            if (Ratio == CardMediaRatio.Square)
+            {
+                yield return "mdc-card__media--square";
+            }
+            else if (Ratio == CardMediaRatio.SixteenByNine)
+            {
+                yield return "mdc-card__media--16-9";
+            }
+        }
+    }
+
+    public enum CardMediaRatio
+    {
+        None,
+        Square,
+        SixteenByNine
+    }
+}
