b4e98a4697e0bf138eeb74f2cd955f9d415352f5
Mahdi <mahdi3466@yahoo.com>
2019-03-24 16:21:14 +0430

    ButtonIcon and ButtonLabel ported to C#.

---
+++ src/Materialium/ButtonIcon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class ButtonIcon : MaterialComponentBase
+    {
+        public ButtonIcon()
+        {
+            Class = "material-icons";
+        }
+
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "i");
+            builder.AddAttribute(n++, "aria-hidden", "true");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-button__icon";
+        }
+    }
+}
---
+++ src/Materialium/ButtonLabel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class ButtonLabel : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "span");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-button__label";
+        }
+    }
+}
