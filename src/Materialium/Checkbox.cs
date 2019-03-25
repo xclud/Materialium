30a1d4ddd92708641956d8b9763544cda5eac1fc
Mahdi <mahdi3466@yahoo.com>
2019-03-25 14:51:09 +0430

    Checkbox and friends ported to C#.

---
+++ src/Materialium/Checkbox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class Checkbox : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "div");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        bool Disabled { get; set; }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-checkbox";
+
+            if (Disabled)
+            {
+                yield return "mdc-checkbox--disabled";
+            }
+        }
+    }
+}
