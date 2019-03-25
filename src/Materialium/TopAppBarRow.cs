a7d33047c32bb9ef4e70fa891abfba0e481040db
Mahdi <mahdi3466@yahoo.com>
2019-03-25 16:19:56 +0430

    TopAppBar and friends ported to C#

---
+++ src/Materialium/TopAppBarRow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Materialium
+{
+    public class TopAppBarRow : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "section");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-top-app-bar__row";
+        }
+    }
+}
---
+++ src/Materialium/TopAppBarSection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Materialium
+{
+    public class TopAppBarSection : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "section");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        bool AlignStart { get; set; }
+
+        [Parameter]
+        bool AlignEnd { get; set; }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-top-app-bar__section";
+
+            if (AlignStart)
+            {
+                yield return "mdc-top-app-bar__section--align-start";
+            }
+
+            if (AlignEnd)
+            {
+                yield return "mdc-top-app-bar__section--align-end";
+            }
+        }
+    }
+}
