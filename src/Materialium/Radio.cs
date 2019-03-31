aad575d3b9c180c7d2f2e2eda1f45ded0c3d2b3c
Mahdi <mahdi3466@yahoo.com>
2019-03-31 16:42:00 +0430

    Radio and friends ported to C#.

---
+++ src/Materialium/Radio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class Radio : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "div");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        bool Disabled { get; set; }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-radio";
+            if (Disabled)
+            {
+                yield return " mdc-radio--disabled";
+            }
+
+        }
+    }
+}
---
+++ src/Materialium/RadioMixedmark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class RadioMixedmark : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "div");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-radio__mixedmark";
+
+        }
+    }
+}
