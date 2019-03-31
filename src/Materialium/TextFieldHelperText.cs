acc5fe53520e5b60b96b581699f572c6cc19275f
Mahdi <mahdi3466@yahoo.com>
2019-03-31 15:26:03 +0430

    TextField and friends ported to C#.

---
+++ src/Materialium/TextFieldHelperText.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Materialium
+{
+    public class TextFieldHelperText : MaterialComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            var n = OpenElementWithCommonAttributes(builder, "div");
+            builder.AddContent(n++, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        bool Persistent { get; set; }
+
+        protected override IEnumerable<string> GetClasses()
+        {
+            yield return "mdc-text-field-helper-text";
+
+            if (Persistent)
+            {
+                yield return "mdc-text-field-helper-text--persistent";
+            }
+
+        }
+    }
+}
